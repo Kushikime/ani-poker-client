601cd2755204996eb69c68d94d835cedeb3877eb
bankai671 <pavelvlas671@gmail.com>
2023-07-23 04:34:41 +0200

    rename: Auth import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import { Button, Input } from '../shared/ui';
 
+import { Auth } from '../features/auth';
+
 export const App = () => {
   return (
     <div className="app green">
-      <div>
-        <Input type="text" placeholder="email or username" />
-        <Input type="password" placeholder="password" />
-        <Button size="medium">Create room</Button>
-      </div>
+      <Auth />
     </div>
   );
 };
