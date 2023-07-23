f1304e2879ea1feface060ed50a1edb977559937
bankai671 <pavelvlas671@gmail.com>
2023-07-23 04:30:55 +0200

    add: input component and styles

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -1,7 +1,17 @@
 import React, { InputHTMLAttributes } from 'react';
 
+import styles from './Input.module.less';
+
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input: React.FunctionComponent<InputProps> = (props) => {
-  return <input {...props} />;
+export const Input: React.FunctionComponent<InputProps> = ({
+  className,
+  ...rest
+}) => {
+  const classNames = React.useMemo(
+    () => [styles.input, className].join(' '),
+    [className]
+  );
+
+  return <input className={classNames} {...rest} />;
 };
