fc2b20e79a00ef8e6eaaeba584338391338cf30b
Kushikime <demcsakerik@gmail.com>
2023-07-23 21:31:04 +0200

    fix: resolved import issues in index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 
 import { AppProvider } from "./providers";
 
-import "./shared/index.less";
+import "../shared/index.less";
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <AppProvider/>
